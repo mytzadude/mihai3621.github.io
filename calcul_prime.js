822dd5a1845aacaa6232fae26896d276dfae049f
mytzadude <puicon.mihai@gmail.com>
2018-10-29 14:42:03 +0200

    4d

--- calcul_prime.js
+++ calcul_prime.js
@@ -1,29 +1,36 @@
-var stopped=false;
+var stopped= false;
 
-onmessage = function(e){
-	if(e.data== "stop"){
-		stopped=true;
+onmessage= function (e)
+{
+	if(e.data == "stop")
+	{
+		stopped= true;
 	}
+	
 }
+
 function is_prim(n){
-	for (var i=2; i*i<=n; ++i)
-		if (n % i == 0)
+	for (var i=2; i*i <= n; i++)
+		if (n % i==0)
 			return false;
-	return true;	
+		return true;
 }
+
 function calculeaza_prime(start_value)
 {
 	if(stopped)
 		;
 	else{
-	for (var i = start_value; i<start_value+1000;i++)
-		if (is_prim(i))
-			postMessage(i);
-	start_value+-1000;
-	}
-		if(start_value< 1e10)
-			setTimeout(calculeaza_prime, 1, start_value);
-		else
-			postMessage("gata");
+
+for  (var i = start_value; i < start_value+1000; i++)
+	if(is_prim(i))
+	postMessage(i);
+start_value +=1000;
+}
+if(start_value< 1e10)
+setTimeout(calculeaza_prime,1,start_value);
+else
+	
+	postMessage("gata");
 }
 calculeaza_prime(1e9);
